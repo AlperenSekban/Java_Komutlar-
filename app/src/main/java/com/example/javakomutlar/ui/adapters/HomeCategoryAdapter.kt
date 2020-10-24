2ec53da2cec56b5ccffcc51d988ee8a13f2f5faa
Alperen <2016010205003@ogrenci.karabuk.edu.tr>
2020-10-24 23:26:52 +0300

    HomceCategoryAdapter is setted

--- app/src/main/java/com/example/javakomutlar/ui/adapters/HomeCategoryAdapter.kt
+++ app/src/main/java/com/example/javakomutlar/ui/adapters/HomeCategoryAdapter.kt
@@ -1,4 +1,44 @@
 package com.example.javakomutlar.ui.adapters
 
-class HomeCategoryAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.javakomutlar.R
+import com.example.javakomutlar.data.models.HomeCategoryModel
+
+class HomeCategoryAdapter (val context: Context,val sendId:SendId): RecyclerView.Adapter<HomeCategoryAdapter.Define>(){
+
+    lateinit var liste: HomeCategoryModel
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeCategoryAdapter.Define {
+        val categoryLayout= LayoutInflater.from(context).inflate(R.layout.layout_home_catogories,parent,false)
+        return Define(categoryLayout)
+    }
+
+    override fun onBindViewHolder(holder: HomeCategoryAdapter.Define, position: Int) {
+        val category=liste.documents
+        holder.categoryTitle.text= category?.get(position)?.fields?.title?.stringValue
+    }
+
+    override fun getItemCount(): Int {
+        return liste.documents!!.size
+    }
+
+    fun setData(liste:HomeCategoryModel){
+        this.liste=liste
+        notifyDataSetChanged()
+    }
+
+    inner class Define(itemview: View):RecyclerView.ViewHolder(itemview){
+        var categoryTitle :TextView
+         init {
+             categoryTitle=itemview.findViewById(R.id.layout_home_catogories_catogory_textView)
+         }
+    }
+
 }
---
+++ app/src/main/java/com/example/javakomutlar/ui/adapters/SendId.kt
@@ -0,0 +1,5 @@
+package com.example.javakomutlar.ui.adapters
+
+interface SendId {
+    fun onItemClick(position:Int)
+}
